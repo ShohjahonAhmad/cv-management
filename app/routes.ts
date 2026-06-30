2b49f57603adeb0906d1653485cd6ce3e76f5fc8
ShohjahonAhmad <shox_0502@mail.ru>
2026-06-30 16:43:41 +0200

    small type on route auth/callback

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"), 
     route("login", "routes/login.tsx"),
-    route("oath/callback", "routes/callback.tsx")
+    route("auth/callback", "routes/callback.tsx")
 ] satisfies RouteConfig;
